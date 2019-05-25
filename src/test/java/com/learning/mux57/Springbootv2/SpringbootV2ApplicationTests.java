516b4c7618254cd7744f961a7a50aaaa6aa1ae40
mux57 <mukeshknit57@gmail.com>
2019-05-25 17:02:42 +0530

    intial commit

---
+++ src/test/java/com/learning/mux57/Springbootv2/SpringbootV2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learning.mux57.Springbootv2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootV2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
